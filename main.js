2db84156569c8a06bac52d82c715a4cf916c9b9a
MechanicalNoob05 <tejasmayekar08@gmail.com>
2022-07-10 22:24:59 +0530

    Typo box, javascripts update

--- main.js
+++ main.js
@@ -2,7 +2,12 @@ const RANDOM_QUOTE_API_URL = "https://api.quotable.io/random"
 const quoteDisplayElement = document.getElementById('quoteDisplay')
 const quoteInputElement = document.getElementById('quoteInput')
 const timerElement = document.getElementById('timer')
-
+const counter = document.getElementById('nooftypos')
+const quotecount = document.getElementById('noofquotes')
+const Averagemistakes = document.getElementById('avg')
+let avgmistake = 0
+let count = 0
+let noofquotes = 0
 quoteInputElement.addEventListener('input', () => {
   const arrayQuote = quoteDisplayElement.querySelectorAll('span')
   const arrayValue = quoteInputElement.value.split('')
@@ -20,6 +25,8 @@ quoteInputElement.addEventListener('input', () => {
       characterSpan.classList.remove('correct')
       characterSpan.classList.add('incorrect')
       correct = false
+      count++
+      counter.innerText = count
     }
   })
   if (correct) renderNewQuote()
@@ -50,6 +57,11 @@ function startTimer() {
   setInterval(() => {
     timer.innerText = getTimerTime()
   }, 1000)
+  noofquotes++
+  quotecount.innerText = noofquotes
+  avgmistake =Math.floor(count/noofquotes)
+  Averagemistakes.innerText = avgmistake
+
 }
 
 function getTimerTime() {
