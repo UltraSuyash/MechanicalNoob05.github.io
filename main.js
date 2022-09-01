9d6f75c29234f05bdd427dd2d8dc522ba68270d2
Tejas <tejasmayekar08@gmail.com>
2022-09-02 01:24:46 +0530

    Dynamic addition of list items

--- main.js
+++ main.js
@@ -1,71 +1,19 @@
-const RANDOM_QUOTE_API_URL = "https://api.quotable.io/random"
-const quoteDisplayElement = document.getElementById('quoteDisplay')
-const quoteInputElement = document.getElementById('quoteInput')
-const timerElement = document.getElementById('timer')
-const counter = document.getElementById('nooftypos')
-const quotecount = document.getElementById('noofquotes')
-const Averagemistakes = document.getElementById('avg')
-let avgmistake = 0
-let count = 0
-let noofquotes = 0
-quoteInputElement.addEventListener('input', () => {
-  const arrayQuote = quoteDisplayElement.querySelectorAll('span')
-  const arrayValue = quoteInputElement.value.split('')
-  let correct = true
-  arrayQuote.forEach((characterSpan, index) => {
-    const character = arrayValue[index]
-    if(character == null){
-      characterSpan.classList.remove('correct')
-      characterSpan.classList.remove('incorrect')
-      correct = false
-    } else if (character === characterSpan.innerText){
-      characterSpan.classList.add('correct')
-      characterSpan.classList.remove('incorrect')
-    } else {
-      characterSpan.classList.remove('correct')
-      characterSpan.classList.add('incorrect')
-      correct = false
-      count++
-      counter.innerText = count
-    }
-  })
-  if (correct) renderNewQuote()
-})
+var one = document.getElementById('first')
+var two = document.getElementById('second')
+var three = document.getElementById('third')
 
-function getRandomQuote() {
-  return fetch(RANDOM_QUOTE_API_URL)
-  .then(response => response.json())
-  .then(data => data.content)
+var divmain = document.createElement('div')
+divmain.classList.add('container')
+var divleft = document.createElement('div')
+divleft.classList.add('left')
+var divright = document.createElement('div')
+divright.classList.add('right')
+
+divmain.appendChild(divleft)
+divmain.appendChild(divright)
+
+function recent(number) {
+  divleft.innerText= 'left' + number
+  divright.innerText= 'right'+ number
+  one.appendChild(divmain)
 }
-async function renderNewQuote() {
-  const quote = await getRandomQuote()
-  quoteDisplayElement.innerHTML = ''
-  quote.split('').forEach(character =>{
-    const characterSpan = document.createElement('span')
-    characterSpan.innerText = character
-    quoteDisplayElement.appendChild(characterSpan)
-
-  })
-  quoteInputElement.value = null
-  startTimer()
-}
-
-let startTime
-function startTimer() {
-  timerElement.innerText = 0
-  startTime = new Date()
-  setInterval(() => {
-    timer.innerText = getTimerTime()
-  }, 1000)
-  noofquotes++
-  quotecount.innerText = noofquotes
-  avgmistake =Math.floor(count/noofquotes)
-  Averagemistakes.innerText = avgmistake
-
-}
-
-function getTimerTime() {
- return Math.floor((new Date() - startTime)/1000) 
-}
-
-renderNewQuote()
