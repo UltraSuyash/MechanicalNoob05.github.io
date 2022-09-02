c7c94d08daad7627c41f0aadc6e439c34f474b41
Tejas <tejasmayekar08@gmail.com>
2022-09-02 17:20:45 +0530

    testing

--- main.js
+++ main.js
@@ -1,19 +1,5 @@
-var one = document.getElementById('first')
-var two = document.getElementById('second')
-var three = document.getElementById('third')
-
-var divmain = document.createElement('div')
-divmain.classList.add('container')
-var divleft = document.createElement('div')
-divleft.classList.add('left')
-var divright = document.createElement('div')
-divright.classList.add('right')
-
-divmain.appendChild(divleft)
-divmain.appendChild(divright)
-
-function recent(number) {
-  divleft.innerText= 'left' + number
-  divright.innerText= 'right'+ number
-  one.appendChild(divmain)
+function edit() {
+  document.getElementById('image11').src = 'room.jpg'
+  document.getElementById('image12').src = 'room.jpg'
+  document.getElementById('image13').src = 'room.jpg'
 }
