f174cf751b0c947f76a2faf464d283a00e56d611
Tejas <tejasmayekar08@gmail.com>
2022-09-10 18:16:15 +0530

    final looks completed it is responsive & complete

--- main.js
+++ main.js
@@ -20,12 +20,37 @@ link.innerText = 'Link to Project'
 var project = [
   ["some other project", "Hello where are u","room.jpg","Extras/Typing_Test/type.html"],
   ["Typing Test","It is a simple web application that renders a random quote, there is an input field which is used to take user input, the value entered from the user is checked against the actual quote, there is a couter which resets as soon as you finish the task.There are various measures of performance are displayed below as soon as they are completed.","room.jpg","Extras/Typing_Test/type.html"],
+  ["Typing Test","It is a simple web application that renders a random quote, there is an input field which is used to take user input, the value entered from the user is checked against the actual quote, there is a couter which resets as soon as you finish the task.There are various measures of performance are displayed below as soon as they are completed.","room.jpg","Extras/Typing_Test/type.html"],
+  ["Typing Test","It is a simple web application that renders a random quote, there is an input field which is used to take user input, the value entered from the user is checked against the actual quote, there is a couter which resets as soon as you finish the task.There are various measures of performance are displayed below as soon as they are completed.","room.jpg","Extras/Typing_Test/type.html"],
+  ["Typing Test","It is a simple web application that renders a random quote, there is an input field which is used to take user input, the value entered from the user is checked against the actual quote, there is a couter which resets as soon as you finish the task.There are various measures of performance are displayed below as soon as they are completed.","room.jpg","Extras/Typing_Test/type.html"],
+  ["Typing Test","It is a simple web application that renders a random quote, there is an input field which is used to take user input, the value entered from the user is checked against the actual quote, there is a couter which resets as soon as you finish the task.There are various measures of performance are displayed below as soon as they are completed.","room.jpg","Extras/Typing_Test/type.html"],
+  ["Typing Test","It is a simple web application that renders a random quote, there is an input field which is used to take user input, the value entered from the user is checked against the actual quote, there is a couter which resets as soon as you finish the task.There are various measures of performance are displayed below as soon as they are completed.","room.jpg","Extras/Typing_Test/type.html"],
+  ["Typing Test","It is a simple web application that renders a random quote, there is an input field which is used to take user input, the value entered from the user is checked against the actual quote, there is a couter which resets as soon as you finish the task.There are various measures of performance are displayed below as soon as they are completed.","room.jpg","Extras/Typing_Test/type.html"],
+  ["Typing Test","It is a simple web application that renders a random quote, there is an input field which is used to take user input, the value entered from the user is checked against the actual quote, there is a couter which resets as soon as you finish the task.There are various measures of performance are displayed below as soon as they are completed.","room.jpg","Extras/Typing_Test/type.html"],
+  ["Typing Test","It is a simple web application that renders a random quote, there is an input field which is used to take user input, the value entered from the user is checked against the actual quote, there is a couter which resets as soon as you finish the task.There are various measures of performance are displayed below as soon as they are completed.","room.jpg","Extras/Typing_Test/type.html"],
+  ["Typing Test","It is a simple web application that renders a random quote, there is an input field which is used to take user input, the value entered from the user is checked against the actual quote, there is a couter which resets as soon as you finish the task.There are various measures of performance are displayed below as soon as they are completed.","room.jpg","Extras/Typing_Test/type.html"],
+  ["Typing Test","It is a simple web application that renders a random quote, there is an input field which is used to take user input, the value entered from the user is checked against the actual quote, there is a couter which resets as soon as you finish the task.There are various measures of performance are displayed below as soon as they are completed.","room.jpg","Extras/Typing_Test/type.html"],
+  ["Typing Test","It is a simple web application that renders a random quote, there is an input field which is used to take user input, the value entered from the user is checked against the actual quote, there is a couter which resets as soon as you finish the task.There are various measures of performance are displayed below as soon as they are completed.","room.jpg","Extras/Typing_Test/type.html"],
+  ["Typing Test","It is a simple web application that renders a random quote, there is an input field which is used to take user input, the value entered from the user is checked against the actual quote, there is a couter which resets as soon as you finish the task.There are various measures of performance are displayed below as soon as they are completed.","room.jpg","Extras/Typing_Test/type.html"],
+  ["Typing Test","It is a simple web application that renders a random quote, there is an input field which is used to take user input, the value entered from the user is checked against the actual quote, there is a couter which resets as soon as you finish the task.There are various measures of performance are displayed below as soon as they are completed.","room.jpg","Extras/Typing_Test/type.html"],
+  ["Typing Test","It is a simple web application that renders a random quote, there is an input field which is used to take user input, the value entered from the user is checked against the actual quote, there is a couter which resets as soon as you finish the task.There are various measures of performance are displayed below as soon as they are completed.","room.jpg","Extras/Typing_Test/type.html"],
+  ["Typing Test","It is a simple web application that renders a random quote, there is an input field which is used to take user input, the value entered from the user is checked against the actual quote, there is a couter which resets as soon as you finish the task.There are various measures of performance are displayed below as soon as they are completed.","room.jpg","Extras/Typing_Test/type.html"],
+  ["Typing Test","It is a simple web application that renders a random quote, there is an input field which is used to take user input, the value entered from the user is checked against the actual quote, there is a couter which resets as soon as you finish the task.There are various measures of performance are displayed below as soon as they are completed.","room.jpg","Extras/Typing_Test/type.html"],
+  ["Typing Test","It is a simple web application that renders a random quote, there is an input field which is used to take user input, the value entered from the user is checked against the actual quote, there is a couter which resets as soon as you finish the task.There are various measures of performance are displayed below as soon as they are completed.","room.jpg","Extras/Typing_Test/type.html"],
+  ["Typing Test","It is a simple web application that renders a random quote, there is an input field which is used to take user input, the value entered from the user is checked against the actual quote, there is a couter which resets as soon as you finish the task.There are various measures of performance are displayed below as soon as they are completed.","room.jpg","Extras/Typing_Test/type.html"],
+  ["Typing Test","It is a simple web application that renders a random quote, there is an input field which is used to take user input, the value entered from the user is checked against the actual quote, there is a couter which resets as soon as you finish the task.There are various measures of performance are displayed below as soon as they are completed.","room.jpg","Extras/Typing_Test/type.html"],
 ]
 var number = project.length
 
 function load(){
    while(view.firstChild){
     view.removeChild(view.lastChild)
+  }
+  while(right.firstChild){
+    right.removeChild(right.lastChild)
+  }
+  while(left.firstChild){
+    left.removeChild(left.lastChild)
   }
  view.appendChild(left)
   view.appendChild(right)
@@ -43,6 +68,12 @@ function recent(){
   while(view.firstChild){
     view.removeChild(view.lastChild)
   }
+  while(right.firstChild){
+    right.removeChild(right.lastChild)
+  }
+  while(left.firstChild){
+    left.removeChild(left.lastChild)
+  }
   view.appendChild(left)
   view.appendChild(right)
   left.appendChild(image)
@@ -59,6 +90,12 @@ function current(){
   while(view.firstChild){
     view.removeChild(view.lastChild)
   }
+  while(right.firstChild){
+    right.removeChild(right.lastChild)
+  }
+  while(left.firstChild){
+    left.removeChild(left.lastChild)
+  }
   view.appendChild(left)
   view.appendChild(right)
   left.appendChild(image)
@@ -74,17 +111,32 @@ function archive(){
   while(view.firstChild){
     view.removeChild(view.lastChild)
   }
+  while(right.firstChild){
+    right.removeChild(right.lastChild)
+  }
+  while(left.firstChild){
+    left.removeChild(left.lastChild)
+  }
   var list = document.createElement('ul')
   list.classList.add('list-group')
-  view.appendChild(list)
+  view.appendChild(left)
+  view.appendChild(right)
+  left.appendChild(image)
+  left.appendChild(link)
+  right.appendChild(list)
   for( let i = 1; i<number; i++){
     var listItem= document.createElement('li')
     listItem.id = 'listItem'+i
     listItem.classList.add('list-group-item')
+    listItem.setAttribute('onclick','random('+i+')')
     list.appendChild(listItem)
   }
   for( let i = 1; i<number; i++){
     var listItem = document.getElementById('listItem'+i)
     listItem.innerText = project[i][0]
   }
+
+}
+function random(input){
+  link.innerText = input
 }
